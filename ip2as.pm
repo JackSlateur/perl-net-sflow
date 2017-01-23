201625cebd2a44406cb76af1d8cc751e7acd1a67
jack <jack@k-net.pro>
2017-01-23 14:17:19 +0100

    New version, completely offline, based on prefetched data from RIPE
    Signed-off-by: jack <jack@k-net.pro>

--- ip2as.pm
+++ ip2as.pm
@@ -6,152 +6,63 @@ use warnings;
 our $VERSION = '1';
 our @EXPORT_OK = qw(getas4ip);
 
-use Net::IP;
-use NetAddr::IP;
+use Data::Dumper;
+use JSON::XS qw(decode_json);
+use Net::Patricia;
 use Socket;
-use Net::DNS::Async;
+use File::Basename;
+use Cwd 'abs_path';
 
-my $res = new Net::DNS::Async(QueueSize => 200, Retries => 1);
-my %IPcache;
-my %IPv6cache;
+sub get_json{
+	my $filename = dirname(abs_path($0)) . '/tools/ip2asn.json';
 
-sub parse{
+	open my $fh, '<', $filename;
+	read $fh, my $string, -s $fh;
+	close $fh;
+
+	return decode_json($string);
+}
+
+sub is_v4{
 	my $ip = shift;
-
-	my $family = undef;
-	my $shortip = undef;
-
 	if(defined(Socket::inet_pton(AF_INET, $ip))){
-		$family = 4;
-		$shortip = NetAddr::IP->new($ip, 24);
-		$shortip = $shortip->network()->addr();
+		return 1;
+	}else{
+		return 0;
 	}
-
-	if(defined(Socket::inet_pton(AF_INET6, $ip))){
-		$family = 6;
-		$shortip = NetAddr::IP->new6($ip, 64);
-		$shortip = $shortip->network()->addr();
-	}
-
-	return $family, $shortip;
 }
 
-sub getcache{
-	my $ip = shift;
+sub get_lookup{
+	my $data = shift;
+	my $v4 = new Net::Patricia;
+	my $v6 = new Net::Patricia AF_INET6;
 
-	my ($family, $shortip) = parse($ip);
-
-	if($family == 4){
-		if(defined($IPcache{$shortip})){
-			return $IPcache{$shortip};
-		}
-	}else{
-		if(defined($IPv6cache{$shortip})){
-			return $IPv6cache{$shortip};
+	foreach my $key(keys %$data){
+		my $value = %$data{$key};
+		my @split = split('/', $key);
+		if(is_v4(shift(@split))){
+			$v4->add_string($key, $value);
+		}else{
+			$v6->add_string($key, $value);
 		}
 	}
-	return;
+	return ($v4, $v6);
 }
 
-sub setcache{
-	my $ip = shift;
-	my $asn = shift;
-
-	my ($family, $shortip) = parse($ip);
-	if($family == 4){
-		$IPcache{$shortip} = $asn;
-	}else{
-		$IPv6cache{$shortip} = $asn;
-	}
-}
-
-sub ptr2ip{
-	my $ptr = shift;
-
-	my $family = 4;
-
-	if($ptr =~ /origin6.asn.cymru.com/){
-		$family = 6;
-	}
-
-	$ptr =~ s/origin.asn.cymru.com//g;
-	$ptr =~ s/origin6.asn.cymru.com//g;
-	chop $ptr;
-
-	my @part = split /\./, $ptr;
-	@part = reverse(@part);
-	if($family == 4){
-		push @part, 0;
-		return join '.', @part;
-	}else{
-		for(my $i = 0; $i < 16; $i++){
-			push @part, 0;
-		}
-
-		my $ip = '';
-		for(my $i = 1; $i < $#part + 2; $i++){
-			$ip .= $part[$i - 1];
-			if($i % 4 == 0){
-				$ip .= ':';
-			}
-		}
-		chop $ip;
-		return $ip;
-	}
-}
-
-sub callback{
-	my $response = shift;
-
-	if(!defined($response)){
-		return;
-	}
-
-	my @question = $response->question;
-	my $real_ip = ptr2ip($question[0]->name);
-
-	foreach my $rr ($response->answer){
-		next unless $rr->type eq 'TXT';
-		my @result = split(/ /, $rr->txtdata);
-		setcache($real_ip, $result[0]);
-		return;
-	}
-	
-}
-
-sub resolve{
-	my $ip = shift;
-	my $truncate = 1;
-
-	my ($family, undef) = parse($ip);
-
-	if($family == 6){
-		$truncate = 16;
-	}
-
-	my $revIP = Net::IP->new($ip)->reverse_ip;
-
-	if($family == 4){
-		$revIP =~ s/in-addr.arpa./origin.asn.cymru.com./g;
-	}else{
-		$revIP =~ s/ip6.arpa./origin6.asn.cymru.com./g;
-	}
-
-	my @part = split /\./, $revIP;
-	$revIP = join '.', @part[$truncate..$#part];
-
-	my $query = new Net::DNS::Packet($revIP, 'TXT');
-	$res->add(\&callback, $query);
-}
+my $data = get_json();
+my ($v4, $v6) = get_lookup($data);
 
 sub getas4ip{
 	my $ip = shift;
-#	print "Debug getas4ip: " . scalar %IPcache . " v4 entries, " . scalar %IPv6cache . " v6 entries, looking for $ip\n";
 
-	my $asn = getcache($ip);
-	if(!defined($asn)){
-		resolve($ip);
+	my $result;
+
+	if(is_v4($ip)){
+		$result = $v4->match_string($ip);
+	}else{
+		$result = $v6->match_string($ip);
 	}
-	return $asn;
+
+	return $result;
 }
 1;
